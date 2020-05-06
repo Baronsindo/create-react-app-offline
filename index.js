c62b58909bff264682f16e7d4b0fcb96ca0c0e0c
adil <adilzakarya8@gmail.com>
2020-05-06 03:20:19 +0000

    unzipping react pre made project to folder

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 const yargs = require("yargs");
 const chalk = require("chalk");
 const boxen = require("boxen");
@@ -18,41 +19,68 @@ const options = yargs
     })
     .argv;
 
-        msg = `
+    // A message to user
+msg = `
     Creat react app offline
 
     Hello! i'm going to make it as fast as i can.`
-    createContent(msg);
+createContent(msg);
 
+// unzip project to folder
+UnzipToFolder(__dirname+'/react-src/react.zip', options.name)
 
+// Just succsess MSG
+function succsessMSG(foldername) {
 
-    // for creating the Header
-    function createHeader() {
-        clear();
-        console.log(
-            chalk.yellow(
-                figlet.textSync('C-R-A-O', {
-                    horizontalLayout: 'full'
-                })
-            )
-        );
+    usefulCMD = chalk.blue.bold('Enjoy, here are some useful commands.') + "\n\n";
+    usefulCMD += "- Go to your project folder \n" + chalk.red.bold('        cd ' + foldername) + '\n\n';
+    usefulCMD += "- Runs the app in development mode. \n" + chalk.red.bold('        npm start ') + '\n\n';
+    usefulCMD += "- Runs the test watcher in an interactive mode. \n" + chalk.red.bold('        npm test') + '\n\n';
+    usefulCMD += "- Builds the app for production to the build folder. \n" + chalk.red.bold('       npm run build') + '\n\n';
+
+    console.log(usefulCMD);
+}
+
+// for unzipping react  to process folder
+function UnzipToFolder(src, foldername) {
+    let target = process.cwd() + '/' + foldername
+    extract(src, target)
+        .then(() => {
+            message = chalk.white.bold('Project Created successfully');
+            createContent(message, true);
+            succsessMSG(foldername);
+        }, err => {
+            createContent('extract failed' + err)
+        })
+}
+
+// for creating the Header
+function createHeader() {
+    clear();
+    console.log(
+        chalk.yellow(
+            figlet.textSync('C-R-A-O', {
+                horizontalLayout: 'full'
+            })
+        )
+    );
+}
+
+// For Creating nice content 
+function createContent(message, wh = false) {
+    if (wh) {
+        createHeader()
     }
 
-    // For Creating nice content 
-    function createContent(message, wh = false) {
-        if (wh) {
-            createHeader()
-        }
-    
-        const boxenOptions = {
-            padding: 1,
-            margin: 1,
-            borderStyle: "round",
-            borderColor: "green",
-            align: "canter"
-        };
-        const msgBox = boxen(message, boxenOptions);
-    
-        console.log(msgBox);
-    
-    }
+    const boxenOptions = {
+        padding: 1,
+        margin: 1,
+        borderStyle: "round",
+        borderColor: "green",
+        align: "canter"
+    };
+    const msgBox = boxen(message, boxenOptions);
+
+    console.log(msgBox);
+
+}
